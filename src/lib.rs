9bede246f79f490a1e2fee3c198216a6ca229b64
Marc Carré <carre.marc@gmail.com>
2020-02-14 10:40:13 +0900

    Set up tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+pub fn problem_0001() -> u32 {
+    2 + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_problem_0001() {
+        assert_eq!(problem_0001(), 4);
     }
 }

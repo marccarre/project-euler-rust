5550ccfb2c609c016756918162c51fd5cd2d7af9
Marc Carré <carre.marc@gmail.com>
2020-02-14 10:54:57 +0900

    Problem 0001: Multiples of 3 and 5

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
-pub fn problem_0001() -> u32 {
-    2 + 2
+/**
+ * Problem 1: Multiples of 3 and 5
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+ * get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+pub fn problem_0001(n: u32) -> u32 {
+    let mut sum = 0;
+    for k in 1..n {
+        if k % 3 == 0 || k % 5 == 0 {
+            sum += k
+        }
+    }
+    return sum;
 }
 
 #[cfg(test)]
@@ -7,6 +19,6 @@ mod tests {
     use super::*;
     #[test]
     fn test_problem_0001() {
-        assert_eq!(problem_0001(), 4);
+        assert_eq!(problem_0001(1000), 233168);
     }
 }

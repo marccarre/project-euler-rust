6e818348ae56eaeebd1f9e816d58b51fd5980eaf
Marc Carré <carre.marc@gmail.com>
2020-04-03 07:34:50 +0900

    Problem 0006: Sum square difference

--- src/lib.rs
+++ src/lib.rs
@@ -112,6 +112,17 @@ pub fn problem_0005(n: u32) -> u32 {
     (1..n).fold(1, |x, y| integer::lcm(x, y))
 }
 
+/**
+ * Problem 6: Sum square difference
+ * The sum of the squares of the first ten natural numbers is: 1**2 + 2**2 + ... + 10**2 = 385.
+ * The square of the sum of the first ten natural numbers is: (1+2+...+10)**2 = 552 = 3025.
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+pub fn problem_0006(n: u32) -> u32 {
+    (1..=n).sum::<u32>().pow(2) - (1..=n).map(|x| x.pow(2)).sum::<u32>()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -142,5 +153,11 @@ mod tests {
     fn test_problem_0005() {
         assert_eq!(problem_0005(10), 2520);
         assert_eq!(problem_0005(20), 232792560);
+    }
+
+    #[test]
+    fn test_problem_0006() {
+        assert_eq!(problem_0006(10), 2640);
+        assert_eq!(problem_0006(100), 25164150);
     }
 }
